20e1c33b58cc269ba0205fa9930ea8eb34a9a4c8
shuaijunlan <shuaijunlan@gmail.com>
2017-12-08 16:01:54 +0800

    update

---
+++ SpringBootLearning/src/main/java/cn/shuaijunlan/springbootlearning/SampleController.java
@@ -0,0 +1,26 @@
+package cn.shuaijunlan.springbootlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Junlan Shuai[shuaijunlan@gmail.com].
+ * @date Created on 15:57 2017/12/8.
+ */
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String home(){
+        return "hello world";
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(SampleController.class);
+    }
+}
