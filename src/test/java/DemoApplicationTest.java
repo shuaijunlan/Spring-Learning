838147f13d8dbcf722fcffd24265bd1d7be61186
shuaijunlan <shuaijunlan@gmail.com>
2017-12-10 14:09:35 +0800

    update

---
+++ src/test/java/DemoApplicationTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Junlan Shuai[shuaijunlan@gmail.com].
+ * @date Created on 14:09 2017/12/10.
+ */
+public class DemoApplicationTest {
+    @Test
+    public void hello() throws Exception {
+    }
+
+}
