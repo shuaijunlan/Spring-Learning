8957ef4dd73d2738afdea175e42ee70485fd5e3a
shuaijunlan <shuaijunlan@gmail.com>
2017-12-10 20:51:32 +0800

    'update'

--- springboot/src/main/java/cn/shuaijunlan/learning/springboot/simplecontroller/SimpleController.java
+++ springboot/src/main/java/cn/shuaijunlan/learning/springboot/simplecontroller/SimpleController.java
@@ -5,22 +5,18 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Junlan Shuai[shuaijunlan@gmail.com].
  * @date Created on 16:00 2017/12/10.
  */
-@Controller
-@EnableAutoConfiguration
+@RestController
 public class SimpleController {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
+    @RequestMapping("/simple")
+    String getString() {
+        return "hello SimpleController1!";
     }
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(SimpleController.class, args);
-    }
 }
--- springboot/src/test/java/cn/shuaijunlan/learning/springboot/simplecontroller/SimpleControllerTest.java
+++ springboot/src/test/java/cn/shuaijunlan/learning/springboot/simplecontroller/SimpleControllerTest.java
@@ -1,19 +1,27 @@
 package cn.shuaijunlan.learning.springboot.simplecontroller;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 
 import static org.junit.Assert.*;
 
 /**
  * @author Junlan Shuai[shuaijunlan@gmail.com].
- * @date Created on 17:20 2017/12/10.
+ * @date Created on 19:23 2017/12/10.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class SimpleControllerTest {
+    @Autowired
+    private SimpleController simpleController1;
 
-    private SimpleController simpleController = new SimpleController();
     @Test
-    public void home() throws Exception {
-        assertEquals("Hello World!",simpleController.home());
+    public void getString() throws Exception {
+        assertEquals("hello SimpleController1!", simpleController1.getString());
     }
 
 }
