e793f2224be7b91ecbb6b307a9dcd402d654f9cd
shuaijunlan <shuaijunlan@gmail.com>
2017-12-10 13:47:35 +0800

    update

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * @author Junlan Shuai[shuaijunlan@gmail.com].
+ * @date Created on 13:12 2017/12/10.
+ */
+public class HelloWorld {
+
+    public String getMessage(boolean bigger) {
+        if (bigger) {
+            return "Hello Universe!";
+        } else {
+            return "Hello World!";
+        }
+    }
+
+}
