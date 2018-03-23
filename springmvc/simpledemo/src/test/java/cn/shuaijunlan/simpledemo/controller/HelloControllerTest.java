e3dc7e933be077ca8d9897eafc068cbd45ece001
shuaijunlan <shuaijunlan@gmail.com>
2018-03-23 10:53:16 +0800

    update

---
+++ springmvc/simpledemo/src/test/java/cn/shuaijunlan/simpledemo/controller/HelloControllerTest.java
@@ -0,0 +1,19 @@
+package cn.shuaijunlan.simpledemo.controller;
+
+import org.junit.Test;
+
+
+/**
+ * @author Junlan Shuai[shuaijunlan@gmail.com].
+ * @date Created on 10:44 2018/3/23.
+ */
+public class HelloControllerTest {
+    @Test
+    public void getHello() throws Exception {
+    }
+
+    @Test
+    public void getStringList() throws Exception {
+    }
+
+}
