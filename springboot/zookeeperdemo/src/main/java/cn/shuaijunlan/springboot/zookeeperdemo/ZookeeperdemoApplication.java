dcc5b2f4c4877686731191751ec6443d7db44ee7
shuaijunlan <shuaijunlan@gmail.com>
2018-03-30 17:14:14 +0800

    update

---
+++ springboot/zookeeperdemo/src/main/java/cn/shuaijunlan/springboot/zookeeperdemo/ZookeeperdemoApplication.java
@@ -0,0 +1,12 @@
+package cn.shuaijunlan.springboot.zookeeperdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeeperdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZookeeperdemoApplication.class, args);
+	}
+}
