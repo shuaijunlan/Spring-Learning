be0d1afc0c510a181b96cbc559f41ade431a2717
Shuai JUnlan <shuaijunlan@gmail.com>
2018-07-26 19:15:44 +0800

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [[ "$1" == "service-register" ]]; then
     echo "start service register center"
-    java -jar ./springcloud/service-register/target/service-register-0.0.1-SNAPSHOT.jar
+    java -jar /root/workspace/springcloud/service-register/target/service-register-0.0.1-SNAPSHOT.jar
 fi
