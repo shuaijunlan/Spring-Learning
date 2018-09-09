fd0f77b69fa316309b6eabe4dac089373ec07457
Shuai Junlan <shuaijunlan@gmail.com>
2018-09-09 12:31:07 +0800

    update

--- spring/spring-beans/src/test/java/cn/shuaijunlan/spring/springbeans/Person1Test.java
+++ spring/spring-beans/src/test/java/cn/shuaijunlan/spring/springbeans/Person1Test.java
@@ -5,6 +5,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -21,6 +23,25 @@ public class Person1Test {
     @Test
     public void getAge() throws Exception {
         Assert.assertEquals(22, person1.getAge());
+    }
+
+    /**
+     * get bean
+     */
+    @Test
+    public void test1(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        Person1 person1 = (Person1) applicationContext.getBean("person1");
+
+    }
+
+    /**
+     * get factory bean
+     */
+    @Test
+    public void test2(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        System.out.println(applicationContext.getBean("&person1"));
     }
 
 }
