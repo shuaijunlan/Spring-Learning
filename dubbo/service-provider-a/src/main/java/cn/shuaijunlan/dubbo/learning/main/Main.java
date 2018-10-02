04a38d846d9d4ea68f89cd798f8644362d77cd37
Shuai Junlan <shuaijunlan@gmail.com>
2018-10-02 20:53:50 +0800

    update

--- dubbo/service-provider-a/src/main/java/cn/shuaijunlan/dubbo/learning/main/Main.java
+++ dubbo/service-provider-a/src/main/java/cn/shuaijunlan/dubbo/learning/main/Main.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 /**
  * @author Junlan Shuai[shuaijunlan@gmail.com].
- * @date Creat             ed on 9:13 PM 2018/07/29.
+ * @date Created on 9:13 PM 2018/07/29.
  */
 public class Main {
     public static void main(String[] args) throws IOException {
