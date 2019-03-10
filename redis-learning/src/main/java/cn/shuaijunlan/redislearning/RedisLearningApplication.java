2f75df5b4d8dd2c06367a348d7eb8c67df27b796
Junlan Shuai <shuaijunlan@gmail.com>
2019-03-10 13:06:44 +0800

    update

---
+++ redis-learning/src/main/java/cn/shuaijunlan/redislearning/RedisLearningApplication.java
@@ -0,0 +1,14 @@
+package cn.shuaijunlan.redislearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisLearningApplication.class, args);
+    }
+
+}
+
---
+++ redis-learning/src/test/java/cn/shuaijunlan/redislearning/RedisLearningApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.shuaijunlan.redislearning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisLearningApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
